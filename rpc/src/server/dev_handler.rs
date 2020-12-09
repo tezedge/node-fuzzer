vlisivka/tezedge

Remove map stats from merkle storage
They were mostly for debugging and do not contain any useful data.
Change RPC endpoint name from "/stats/database_mem" to "/stats/context" for
a better description.

rpc/src/server/dev_handler.rs
@@ -69,9 +69,9 @@ pub async fn dev_stats_memory(_: Request<Body>, _: Params, _: Query, env: RpcSer
     }
 }
 
-pub async fn database_memstats(_: Request<Body>, _: Params, _: Query, env: RpcServiceEnvironment) -> ServiceResult {
+pub async fn context_stats(_: Request<Body>, _: Params, _: Query, env: RpcServiceEnvironment) -> ServiceResult {
     result_to_json_response(
-        base_services::get_database_memstats(env.tezedge_context()),
+        base_services::get_context_stats(env.tezedge_context()),
         env.log(),
     )
 }

tezedge/node-fuzzer

rpc/src/server/dev_handler.rs
@@ -108,9 +108,9 @@ pub async fn dev_stats_memory(_: Request<Body>, _: Params, _: Query, env: RpcSer
     }
 }
 
-pub async fn database_memstats(_: Request<Body>, _: Params, _: Query, env: RpcServiceEnvironment) -> ServiceResult {
+pub async fn context_stats(_: Request<Body>, _: Params, _: Query, env: RpcServiceEnvironment) -> ServiceResult {
     result_to_json_response(
-        dev_services::get_database_memstats(env.tezedge_context()),
+        dev_services::get_context_stats(env.tezedge_context()),
         env.log(),
     )
 }