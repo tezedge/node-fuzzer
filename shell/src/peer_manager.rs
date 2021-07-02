9dfcc63f3a3e8c1f17807f4f51f99550c075ad89
Zura Benashvili <zura.benashvili@gmail.com>
2021-07-02 17:29:31 +0400

    feat(tezedge_state): implement effects

--- shell/src/peer_manager.rs
+++ shell/src/peer_manager.rs
@@ -288,6 +288,7 @@ impl Actor for PeerManager {
             },
             (*self.local_node_info.identity()).clone(),
             (*self.local_node_info.version()).clone(),
+            Default::default(),
             Instant::now(),
         );
 
