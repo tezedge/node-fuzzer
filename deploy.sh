8d6975a7e1c925d5916f648f036d636e3736d6cd
Daniel Kuehr <daniel.kuehr@viablesystems.io>
2021-10-20 11:58:10 -0300

    add more restrictive iptables rules

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ cargo build --release
 docker network create --subnet=172.18.0.0/16 fuzznet
 ip -4 route add local 172.28.0.0/16 dev lo
 docker build -t fuzz_target .
-iptables -I DOCKER-USER -s 172.18.0.101 -p tcp --dport 9732 -j REJECT --reject-with tcp-reset
+iptables -I DOCKER-USER -s 172.18.0.101 -p tcp --tcp-flags SYN,ACK SYN -j REJECT --reject-with tcp-reset
+iptables -I DOCKER-USER -s 172.18.0.101 -p tcp --dport 443 -j ACCEPT
+iptables -I DOCKER-USER -s 172.18.0.101 -p tcp --dport 80 -j ACCEPT
